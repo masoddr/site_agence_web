aa94606bddbd017a519604b067ad5ff4c0065645
masoddr <ouaddour.massyl@gmail.com>
2025-06-03 23:19:46 +0200

    Optim

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,39 +4,5 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  build: {
-    // Optimisations de build
-    inlineStylesheets: 'auto',
-    assets: '_assets',
-    format: 'directory'
-  },
-  vite: {
-    build: {
-      // Optimisations Vite
-      cssMinify: true,
-      minify: true,
-      rollupOptions: {
-        output: {
-          manualChunks: {
-            'vendor': ['react', 'react-dom'],
-            'styles': ['/styles/global.css']
-          }
-        }
-      }
-    }
-  },
-  compressHTML: true,
-  image: {
-    // Configuration des images
-    service: {
-      entrypoint: 'astro/assets/services/sharp'
-    },
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**'
-      }
-    ]
-  }
+  integrations: [tailwind()]
 });
